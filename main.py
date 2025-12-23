85924a853b00b46ffbd216adc10149f1074bea3d
Hoang271205 <hoang.pham230217@vnuk.edu.vn>
2025-12-23 13:11:43 +0700

    giai thich

--- main.py
+++ main.py
@@ -29,12 +29,9 @@ except:
 if __name__ == "__main__":
     obfuscated_blob = obfuscate_payload(PAYLOAD, SECRET_KEY)
     
-    # Lưu bằng chứng phân tích tĩnh
     os.makedirs("output", exist_ok=True)
     with open("output/obfuscated_payload.bin", "wb") as f:
         f.write(obfuscated_blob)
 
-    # 2. QUÁ TRÌNH STUB (Sẽ chạy khi nạn nhân click vào file)
-    # Trong môi trường thực tế, phần 'obfuscated_blob' sẽ được nhúng cứng vào file exe
     deobfuscate_and_run(obfuscated_blob, SECRET_KEY)
-
+# file này chỉ để mô phỏng quá trình đóng gói và chạy payload đã được obfuscate.
